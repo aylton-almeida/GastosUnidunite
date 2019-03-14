9691f5b20c80442741468b7e54e9d702e5433f4d
stevreeper <ayltonalmeida2@gmail.com>
2019-03-14 09:13:33 -0300

    construindoTela

--- src/controllers/YourAccountController.java
+++ src/controllers/YourAccountController.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
@@ -10,9 +15,22 @@ public class YourAccountController extends MainController implements Initializab
 
 
     public Label labelBemVindo;
+    public JFXTextField emailInput;
+    public JFXPasswordField passInput;
+    public JFXTextField confPassInput;
+    public JFXButton updateDataBtn;
+    public JFXButton createUser;
+    public JFXCheckBox isAdminInput;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        labelBemVindo.setText(super.getLoggedUser().getEmail());
+
     }
 
+    public void updateData(ActionEvent event) {
+    }
+
+    public void createUser(ActionEvent event) {
+    }
 }
