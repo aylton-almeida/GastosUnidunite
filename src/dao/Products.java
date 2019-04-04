e3bf410a78bd0f26f03462b48ac3f3e1338fc111
Aylton Almeida <ayltonalmeida2@gmail.com>
2019-04-04 09:21:55 -0300

    add product no sql

--- src/dao/Products.java
+++ src/dao/Products.java
@@ -1,68 +1,75 @@
 package dao;
 
 import interfaces.Dao;
+import logic.Client;
 import logic.Product;
 
 import java.io.RandomAccessFile;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class Products implements Dao<Product> {
 
-    private String fileName;
+    private String host = "bancounidunite.mysql.database.azure.com";
+    private String database = "unidunite";
+    private String user = "AyltonJunior@bancounidunite";
+    private String password = "Aylton123";
+    private Connection connection = null;
 
-    public Products(){
-        setFileName("products");
+    public Products() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = String.format("jdbc:mysql://%s/%s", host, database);
+
+        //Set connection properties
+        Properties properties = new Properties();
+        properties.setProperty("user", user);
+        properties.setProperty("password", password);
+        properties.setProperty("useSSL", "true");
+        properties.setProperty("verifyServerCertificate", "true");
+        properties.setProperty("requireSSL", "false");
+
+        connection = DriverManager.getConnection(url, properties);
+
+        if (connection == null)
+            throw new Exception("Failed to create connection to database.");
     }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
     @Override
     public List<Product> getAllObjects() throws Exception {
-        RandomAccessFile file = new RandomAccessFile(getFileName(), "r");
-        List<Product> productList = new ArrayList();
-        int actualPoint = 0;
-        while (actualPoint < file.length()) {
-            int size = file.readInt();
-            byte b[] = new byte[size];
-            file.read(b);
-            productList.add((Product) new Product().setByteArray(b));
-            actualPoint += 4 + size;
+        List<Product> list = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery("SELECT * from tbl_product;");
+        while (results.next()) {
+            System.out.println(results.getInt(1));
+            System.out.println(results.getString(2));
+            System.out.println(results.getString(3));
+
+            Product product = new Product(results.getInt(1), results.getString(2), results.getString(5), results.getString(4), results.getDouble(3));
+            list.add(product);
         }
-        file.close();
-        return productList;
+        return list;
     }
 
     @Override
     public Product getObject(Object key) throws Exception {
-        RandomAccessFile file = new RandomAccessFile(getFileName(), "r");
-        int actualPoint = 0;
-        while (actualPoint < file.length()) {
-            int size = file.readInt();
-            byte b[] = new byte[size];
-            file.read(b);
-            Product product = (Product) new Product().setByteArray(b);
-            if (product.getId() == (int) key)
-                return product;
-            actualPoint += 4 + size;
-        }
-        file.close();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from tbl_product WHERE id = ?;");
+        preparedStatement.setInt(1, (int) key);
+        ResultSet results = preparedStatement.executeQuery();
+        if (results.next())
+            return new Product(results.getInt(1), results.getString(2), results.getString(5), results.getString(4), results.getDouble(3));
         return null;
     }
 
     @Override
     public void addObject(Product o) throws Exception {
-        RandomAccessFile file = new RandomAccessFile(getFileName(), "rw");
-        file.seek(file.length());
-        file.writeInt(o.getByteArray().length);
-        file.write(o.getByteArray());
-        file.close();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO tbl_product (id, name, value, size, factory) VALUES (?, ?, ?, ?, ?);");
+        preparedStatement.setInt(1, o.getId());
+        preparedStatement.setString(2, o.getName());
+        preparedStatement.setDouble(3, o.getValue());
+        preparedStatement.setString(4, o.getSize());
+        preparedStatement.setString(5, o.getFactory());
+        preparedStatement.executeUpdate();
     }
 
     @Override
